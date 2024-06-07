3719d81bd762d92385db8c395c010d72cb2d536a
ZakirAliyev <aliyevzakir814@gmail.com>
2024-06-08 03:26:13 +0400

    asdasd

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,40 +1,46 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import './Navbar.scss'
-function Navbar() {
-  return (
-    <>
-      <nav>
-        <div className="navbar-general">
-          <div className="navbar-logo"><img className="navbar-logo-img" src="https://alovservisimages.netlify.app/img/whitelogo.png" alt="" /></div>
-          <ul className="navbar-items">
-            <Link to={'/'}>
-              <li>Ana səhifə</li>
-            </Link >
-            <Link to={'/combimaster'}>
-              <li>Kombi ustası</li>
-            </Link>
-            <Link to={'combibrains'}>
-              <li>Kombi plataları</li>
-            </Link>
-            <Link to={'salesofparts'}>
-              <li>Ehtiyat hissələrin satışı</li>
-            </Link>
-            <Link to={'contactus'}>
-              <li>Bizimlə Əlaqə</li>
-            </Link>
+import {FaBars} from "react-icons/fa";
+import NavbarBurger from "../NavbarBurger/index.jsx";
 
-            <select name="language">
-                <option value="Az">Az</option>
-                <option value="Tr">Tr</option>
-                <option value="Ru">Ru</option>
-                <option value="Eng">Eng</option>
-              </select>
-          </ul>
-        </div>
-      </nav>
-    </>
-  );
+function Navbar() {
+    return (
+        <>
+            <nav>
+                <div className="navbar-general">
+                    <div className="navbar-logo"><img className="navbar-logo-img"
+                                                      src="https://alovservisimages.netlify.app/img/whitelogo.png"
+                                                      alt=""/></div>
+                    <NavbarBurger/>
+                    <ul className="navbar-items">
+                        <Link to={'/'}>
+                            <li>Ana səhifə</li>
+                        </Link>
+                        <Link to={'/combimaster'}>
+                            <li>Kombi ustası</li>
+                        </Link>
+                        <Link to={'combibrains'}>
+                            <li>Kombi plataları</li>
+                        </Link>
+                        <Link to={'salesofparts'}>
+                            <li>Ehtiyat hissələrin satışı</li>
+                        </Link>
+                        <Link to={'contactus'}>
+                            <li>Bizimlə Əlaqə</li>
+                        </Link>
+
+                        <select name="language">
+                            <option value="Az">Az</option>
+                            <option value="Tr">Tr</option>
+                            <option value="Ru">Ru</option>
+                            <option value="Eng">Eng</option>
+                        </select>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    );
 }
 
 export default Navbar;
--- src/components/WebsiteTop/Top.jsx
+++ src/components/WebsiteTop/Top.jsx
@@ -1,28 +1,35 @@
 import React from "react";
 import "./Top.scss";
-function Top() {
-  return (
-    <>
-      <section className="topbar">
-      
-          <div className="topbar-items">
-            <div className="topbar-left">
-              <p>Xırdalan şəhəri,Mehdi Hüseynzadə küç №26</p>
-              <p>055-600-84-83</p>
-              <p>info@alovservis.az</p>
-            </div>
-            <div>
-              <i class="fa-brands fa-facebook"></i>
-              <i class="fa-brands fa-facebook"></i>
-              <i class="fa-brands fa-facebook"></i>
-              <i class="fa-brands fa-facebook"></i>
-              <i class="fa-brands fa-facebook"></i>
-            </div>
-          </div>
+import {FaHome, FaPhone, FaSupple} from "react-icons/fa";
+import {FaLocationDot} from "react-icons/fa6";
 
-      </section>
-    </>
-  );
+function Top() {
+    return (
+        <>
+            <section className="topbar">
+
+                <div className="topbar-items">
+                    <div className="topbar-left">
+                        <p>Xırdalan şəhəri,Mehdi Hüseynzadə küç №26</p>
+                        <p>055-600-84-83</p>
+                        <p>info@alovservis.az</p>
+                    </div>
+                    <div style={{display: 'flex', alignItems: 'center', gap: '20px', fontSize: '20px'}}>
+                        <a href={'/'} style={{color: 'white'}}>
+                            <FaHome/>
+                        </a>
+                        <a href={'/'} style={{color: 'white'}}>
+                            <FaPhone/>
+                        </a>
+                        <a href={'/'} style={{color: 'white'}}>
+                            <FaLocationDot/>
+                        </a>
+                    </div>
+                </div>
+
+            </section>
+        </>
+    );
 }
 
 export default Top;
